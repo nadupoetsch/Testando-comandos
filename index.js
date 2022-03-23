f311b7f0dff90b4040bb41c41d29805efe2c8877
Bernardo Poetsch <89049723+nadupoetsch@users.noreply.github.com>
2022-03-23 02:56:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// Testando os comandos conforme é ensinado
+
+alert ("") //comando para dar um espaço de uma linha
+
+// 'alert'
+  // Comando utilizado para mostrar uma frase
+  alert ("Este um sistema para ir inserindo os comandos que aprendemos conforme as aulas estão sendo dadas")
+  alert ("")
+  alert ("Este comando é o alert, utilizado para aparecer a frase que estás lendo")
+//
+
+alert ("")
+
+// 'var' é uma variável que pode ter um valor inserido
+
+  var nome
+  nome = prompt("digite seu nome: ");
+
+  // ou também => var nome = prompt("digite seu nome: ");
+
+ alert("seu nome é: "+ nome +".")
+ //utilizar o + para ligar a variável com o texto, cuidar bem das "
+//
+
+alert ("")
+
+// Utilização do 'parseInt' para transformar o caractere em numeral inteiro e poder calcular
+
+  var numeroA = parseInt(prompt('Digite o número A'))
+  var numeroB = parseInt(prompt('Digite o número B'))
+  var numeroC = numeroA + numeroB
+
+  alert('número A + número B = ' + numeroC)
+//
+
+alert ("")
+
+//
